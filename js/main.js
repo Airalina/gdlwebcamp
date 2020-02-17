67f49d98825be113bdb65df924e954d80d3ae8e6
Airalina <airalinac@gmail.com>
2020-02-17 18:16:43 -0400

    commit8

--- js/main.js
+++ js/main.js
@@ -29,6 +29,38 @@
     pase_dosdias.addEventListener('blur', mostrarDias);
     pase_completo.addEventListener('blur', mostrarDias);
 
+    nombre.addEventListener('blur', validarCampos);
+    apellido.addEventListener('blur', validarCampos);
+    email.addEventListener('blur', validarCampos);
+    email.addEventListener('blur', validarMail);
+
+      function validarCampos(){
+        if(this.value == '') {
+          errorDiv.style.display = "block";
+          errorDiv.innerHTML = "Este campo es obligatorio";
+          this.style.border ='1px solid red'; 
+          errorDiv.style.border = '1px solid red';
+        } else {
+          errorDiv.style.display = 'none';
+          this.style.border = '1px solid #cccccc';
+        }
+      }
+
+      function validarMail(){
+        if(this.value.indexOf("@") > -1) {
+          errorDiv.style.display = 'none';
+          this.style.border = '1px solid #cccccc';
+        }
+        else {
+          errorDiv.style.display = "block";
+          errorDiv.innerHTML = "Debe tener un arroba";
+          this.style.border ='1px solid red'; 
+          errorDiv.style.border = '1px solid red';
+        }
+      }
+
+
+
     function calcularMontos(event) {
       event.preventDefault();
       // console.log("Has hecho click en calcular");
