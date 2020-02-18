493ef10f06ef30578d06a8e5939daf34f31a8dba
Airalina <airalinac@gmail.com>
2020-02-18 15:22:19 -0400

    commitnuevo

--- js/main.js
+++ js/main.js
@@ -137,29 +137,27 @@
       }
     }
 
-
-
     //console.log("listo");
-
-
-
-
-
-
-
-
   }); //DOM CONTENT LOADED
-
-
-
-
-
-
-
-
-
-
-
-
-
 })();
+
+$(function(){
+  //programa de conferencias
+  $('.programa-evento .info-curso:first').show();
+  $('nav.menu-programa  a:first').addClass('activo');
+  $('nav.menu-programa a').on('click', function(){
+    $('nav.menu-programa a').removeClass('activo');
+    $(this).addClass('activo');
+  $('.ocultar').hide();
+  var enlace= $(this).attr('href');
+  //console.log(enlace);
+  $(enlace).fadeIn(1000); //es 1 seg
+    return false;
+  });
+
+  //animaciones para los numeros
+  $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6}),1200;
+  $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15}),1200;
+  $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3}),1500;
+  $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9}),1800;
+});
