8c5f16d47e5c5b21bed6d4070fd6ad3fda05d745
Airalina <airalinac@gmail.com>
2020-02-20 12:21:13 -0400

    commit3

--- calendario.php
+++ calendario.php
@@ -3,45 +3,54 @@
 
 
 <section class="seccion contenedor">
-  <h2>Calendario de eventos</h2>
-  
-  <?php 
-  
-  try{
+    <h2>Calendario de eventos</h2>
+
+    <?php
+
+    try {
         require_once('includes/funciones/bd_conexion.php');
         $sql = "SELECT * FROM eventos";
         $resultado = $conn->query($sql);
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
 
-  } catch (\Exception $e) {
-      echo $e->getMessage();
-  }
-  
-  ?>
-<div class="calendario">
-<?php 
-   $eventos= $resultado->fetch_assoc();
-?>
-<pre>
-<?php 
-  var_dump($eventos);
+    ?>
+    <div class="calendario">
+        <?php
+        while ($eventos = $resultado->fetch_assoc()) { 
+         
+                
+
+
+       //  echo $eventos['nombre_evento'];
+         //           echo "<br>";
+      
+          //  var_dump($eventos);
+        ?>
+
+        <?php } ?>
+        <pre>
+<?php
+//var_dump($eventos);
 ?>
 </pre>
 
 
-</div>
-<?php 
-  $conn->close();
-?>
+    </div>
+    <?php
+    $conn->close();
+    ?>
 </section>
 <!--seccion Galeria-->
 <?php include_once 'includes/templates/footer.php'; ?>
 <script src="js/vendor/modernizr-3.8.0.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script>
-  window.jQuery ||
-    document.write(
-      '<script src="js/vendor/jquery-3.4.1.min.js"><\/script>'
-    );
+    window.jQuery ||
+        document.write(
+            '<script src="js/vendor/jquery-3.4.1.min.js"><\/script>'
+        );
 </script>
 <script src="js/plugins.js"></script>
 <script src="js/jquery.animateNumber.js"></script>
@@ -53,14 +62,14 @@
 
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>
-  window.ga = function() {
-    ga.q.push(arguments);
-  };
-  ga.q = [];
-  ga.l = +new Date();
-  ga("create", "UA-XXXXX-Y", "auto");
-  ga("set", "transport", "beacon");
-  ga("send", "pageview");
+    window.ga = function() {
+        ga.q.push(arguments);
+    };
+    ga.q = [];
+    ga.l = +new Date();
+    ga("create", "UA-XXXXX-Y", "auto");
+    ga("set", "transport", "beacon");
+    ga("send", "pageview");
 </script>
 <script src="https://www.google-analytics.com/analytics.js" async></script>
 </body>
