b10d1d01c31ace54c1cfff83121231debd8cf7ea
Airalina <airalinac@gmail.com>
2020-02-20 12:46:13 -0400

    commit4

--- calendario.php
+++ calendario.php
@@ -9,7 +9,8 @@
 
     try {
         require_once('includes/funciones/bd_conexion.php');
-        $sql = "SELECT * FROM eventos";
+        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado FROM eventos INNER JOIN categoria_evento ON eventos.id_cat_evento = categoria_evento.id_categoria INNER JOIN invitados ON eventos.id_inv = invitados.invitado_id ORDER BY evento_id";
+
         $resultado = $conn->query($sql);
     } catch (\Exception $e) {
         echo $e->getMessage();
@@ -18,25 +19,21 @@
     ?>
     <div class="calendario">
         <?php
-        while ($eventos = $resultado->fetch_assoc()) { 
-         
-                
-
-
-       //  echo $eventos['nombre_evento'];
-         //           echo "<br>";
-      
-          //  var_dump($eventos);
+        while ($eventos = $resultado->fetch_assoc()) {   //  echo $eventos['nombre_evento'];
+            //           echo "<br>";   
         ?>
+            <pre>
+        <?php
+            var_dump($eventos);
+        ?>
+            </pre>
+        <?php    } ?>
 
-        <?php } ?>
         <pre>
 <?php
 //var_dump($eventos);
 ?>
 </pre>
-
-
     </div>
     <?php
     $conn->close();
